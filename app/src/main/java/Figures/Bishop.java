f72e02d2fbf9e72976e1da7cea35e64c197d736f
DanielS04 <daniel.stasielak@gmail.com>
2025-01-04 18:30:01 +0100

    Goniec zrobiony

--- app/src/main/java/Figures/Bishop.java
+++ app/src/main/java/Figures/Bishop.java
@@ -9,16 +9,57 @@ public class Bishop extends Figure {
     value = 3;
   }
   public List< List< Integer >> getPossibleMoves( GameManager.GM gm ){
-        List< List< Integer >> moves = GameManager.Helpers.InitializeArray8x8();
-        List< List< Integer >> board = gm.getBoard();
+    List< List< Integer >> moves = GameManager.Helpers.InitializeArray8x8();
+    List< List< Integer >> board = gm.getBoard();
 
-        if( this.isWhite ){
+    // x+ y+
+    for (int i = 1; ( x+i<8 && y+i<8 ); i++) {
+      if( board.get( y+i ).get( x+i ) == 69 )
+            moves.get( y+i ).set( x+i, 1 );
+          else if ( board.get( y+i ).get( x+i ) != 69 && gm.getPieceAt( x+i, y+i ).getTeam() != this.isWhite ){
+            moves.get( y+i ).set( x+i, 2 );
+            break;
+          }
+          else
             ;
-        } else {
-            ;
-        }
-
-
-        return moves;
     }
+    // x+ y-
+    for (int i = 1; ( x+i<8 && y-i>=0 ); i++) {
+      if( board.get( y-i ).get( x+i ) == 69 )
+            moves.get( y-i ).set( x+i, 1 );
+          else if ( board.get( y-i ).get( x+i ) != 69 && gm.getPieceAt( x+i, y-i ).getTeam() != this.isWhite ){
+            moves.get( y-i ).set( x+i, 2 );
+            break;
+          }
+          else
+            ;
+    }
+    // x- y+
+    for (int i = 1; ( x-i>=0 && y+i<8 ); i++) {
+      if( board.get( y+i ).get( x-i ) == 69 )
+            moves.get( y+i ).set( x-i, 1 );
+          else if ( board.get( y+i ).get( x-i ) != 69 && gm.getPieceAt( x-i, y+i ).getTeam() != this.isWhite ){
+            moves.get( y+i ).set( x-i, 2 );
+            break;
+          }
+          else
+            ;
+    }
+    // x- y-
+    for (int i = 1; ( x-i>=0 && y-i>=0 ); i++) {
+      if( board.get( y-i ).get( x-i ) == 69 )
+            moves.get( y-i ).set( x-i, 1 );
+          else if ( board.get( y-i ).get( x-i ) != 69 && gm.getPieceAt( x-i, y-i ).getTeam() != this.isWhite ){
+            moves.get( y-i ).set( x-i, 2 );
+            break;
+          }
+          else
+            ;
+    }
+
+
+
+
+    return moves;
+  }
 }
