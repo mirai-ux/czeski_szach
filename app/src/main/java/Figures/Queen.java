b29af3f32ebc15869e46b50c231559a2179b792d
DanielS04 <daniel.stasielak@gmail.com>
2025-01-04 20:02:14 +0100

    Hetman ogarniety

--- app/src/main/java/Figures/Queen.java
+++ app/src/main/java/Figures/Queen.java
@@ -10,14 +10,16 @@ public class Queen extends Figure{
     }
     public List< List< Integer >> getPossibleMoves( GameManager.GM gm ){
         List< List< Integer >> moves = GameManager.Helpers.InitializeArray8x8();
-        List< List< Integer >> board = gm.getBoard();
 
-        if( this.isWhite ){
-            ;
-        } else {
-            ;
-        }
+        Rook r = new Rook(this.x, this.y, this.isWhite);
+        List< List< Integer >> movesRook = GameManager.Helpers.InitializeArray8x8();
+        Bishop b = new Bishop(this.x, this.y, this.isWhite);
+        List< List< Integer >> movesBishop = GameManager.Helpers.InitializeArray8x8();
 
+        movesRook = r.getPossibleMoves(gm);
+        movesBishop = b.getPossibleMoves(gm);
+
+        moves = GameManager.Helpers.mergeTwoArrays8x8(movesRook, movesBishop);
 
         return moves;
     }
