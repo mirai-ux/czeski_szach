6bbf29a7a4d9041e2b9dbc66579fe76c56b481ad
DanielS04 <daniel.stasielak@gmail.com>
2025-01-12 17:13:51 +0100

    poruszanie pionkami prawie zrobione

--- app/src/main/java/GameManager/GM.java
+++ app/src/main/java/GameManager/GM.java
@@ -15,29 +15,6 @@ public class GM {
   private String pieceDestination;
 
   public GM() {
-    /*
-    // System.out.println("GM");
-    helper = new Helpers();
-    // selectedPieceId = -1;
-    isTurnWhite = true;
-    asddddddd
-    // Add option to choose save for load
-    AllFigures = helper.ReadFromFile("testTemplate.txt");
-    board = helper.InitializeArray8x8();
-    board = helper.UpgradeArray8x8(AllFigures, board);
-    helper.printArray8x8(board);
-    System.out.println("\n");
-
-    
-    selectedPiece = getPieceAt(4,2);
-    System.out.println(selectedPiece.getType());
-    System.out.println(selectedPiece.getTeam ());
-
-    System.out.println(helper.ArrayToFEN( this ));
-
-    possibleMoves = selectedPiece.getPossibleMoves(this);
-    helper.printArray8x8(possibleMoves);
-     */
     isTurnWhite = true;
     helper = new Helpers();
 
@@ -47,44 +24,49 @@ public class GM {
     possibleMoves = helper.InitializeArray8x8();
 
   }
+
+  //  1. Na szachownicy wybrać pionka i zaktualizowac tablice na mozliwe ruchy
+  //       getPossibilities( xPola, yPola )
+  public List<List<Integer>> getPossibilities ( int x_, int y_ ){
+    selectedPiece = getPieceAt( x_, y_ );
+    possibleMoves = selectedPiece.getPossibleMoves( this );
+    piecePosition = "" + (char)( x_ + 97 ) + ( 8 - y_ );
+    System.out.println( "Pozycja wybranej figury: " + piecePosition );
+    return possibleMoves;
+  }
+  //  2. Wybranie pola wywoluje selectDestination( xPola, yPola )
+  public void selectDestination( int x_, int y_ ){
+    pieceDestination = "" + (char)( x_ + 97 ) + ( 8 - y_ );
+    makeMove( piecePosition + pieceDestination );
+  }
+  // 3. Poźniej trzeba od nowa pobrać tablice z figurami od GM'a getBoard()
+
   // getters
   public List<List<Integer>> getBoard(){ return board; }
   public List<Figure> getFigures(){ return AllFigures; }
   public boolean getTurn(){ return isTurnWhite; }
 
   public Figure getPieceAt( int x_, int y_ ){
-    int selectedPieceId = board.get( y_ ).get( x_ );
-    return AllFigures.get( selectedPieceId );
-  }
-  public List<List<Integer>> getPossibilities ( int x_, int y_ ){
-    selectedPiece = getPieceAt( x_, y_ );
     selectedPieceId = board.get( y_ ).get( x_ );
-    possibleMoves = selectedPiece.getPossibleMoves( this );
-    piecePosition = "" + (char)( x_ + 97 ) + ( 8 - y_ );
-    return possibleMoves;
-  }
-  public void selectDestination( int x_, int y_ ){
-    pieceDestination = "" + (char)( x_ + 97 ) + ( 8 - y_ );
-    makeMove( piecePosition + pieceDestination );
+    return AllFigures.get( selectedPieceId );
   }
 
   private void makeMove( String move ){
-    int xPos = (int) move.charAt( 0 ) - 97;
-    int yPos = (int) move.charAt( 1 ) - 52;
-    int xDest = (int) move.charAt( 2 ) - 97;
-    int yDest = (int) move.charAt( 3 ) - 52;
-    System.out.println( "\n " + xPos + yPos  );
-    if( board.get(yDest).get(xDest) == 69 ){
-      selectedPiece.setPosition( xDest, yDest );
-      board.get( yPos ).set( xPos, 69 );
-      board.get( yDest ).set( xDest, selectedPieceId );
-    } else {
-      Figure poorFigure = getPieceAt( xDest, yDest );
-      poorFigure.setActivity( false );
-      selectedPiece.setPosition( xDest, yDest );
-      board.get( yPos ).set( xPos, 69 );
-      board.get( yDest ).set( xDest, selectedPieceId );
-    }
+    int xPos = move.charAt( 0 ) - 'a';
+    int yPos = 8 - move.charAt( 1 ) + '0';
+    int xDest = move.charAt( 2 ) - 'a';
+    int yDest = 8 - move.charAt( 3 ) + '0';
+
+    int currentID = board.get( yPos ).get( xPos );
+
+    // System.out.println( "\nPos: ( " + xPos + ", " + yPos + " ).");
+    // System.out.println( currentID );
+    // System.out.println( "Dest: ( " + xDest + ", " + yDest + " ).");
+
+    Figure poorFigure = getPieceAt(xDest, yDest);
+    poorFigure.setActivity( false );
+    board.get( yPos ).set( xPos, 69);
+    board.get( yDest ).set( xDest, currentID );
   }
 
   public void tests(){
@@ -94,13 +76,15 @@ public class GM {
     selectedPiece = getPieceAt( x_, y_ );
     System.out.println(selectedPiece.getType());
     System.out.println(selectedPiece.getTeam ());
-    piecePosition = "" + (char)( x_ + 97 ) + ( 8 - y_ );
+    //piecePosition = "" + (char)( x_ + 97 ) + ( 8 - y_ );
     System.out.println(helper.ArrayToFEN( this ));
-    System.out.println( piecePosition );
+    System.out.println( selectedPieceId );
 
-    possibleMoves = selectedPiece.getPossibleMoves(this);
+    possibleMoves = getPossibilities(x_, y_);
     helper.printArray8x8(possibleMoves);
 
-    makeMove( "e6e5" );
+    makeMove( "e6f7" );
+
+    helper.printArray8x8(board);
   }
 }
